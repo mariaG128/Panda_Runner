5fa491e70a70689002a640b995bd8f681a59d1a5
Michael Zhang <heronalps@gmail.com>
2020-01-19 00:57:37 -0800

    mz - Refactor index.js

--- index.js
+++ index.js
@@ -8,41 +8,35 @@ var firebaseConfig = {
     messagingSenderId: "8929190603",
     appId: "1:8929190603:web:4f5cd0feb3d9d665c2f14e",
     measurementId: "G-GE4LPT8MMZ"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
-  let auth = firebase.auth();
-  let database = firebase.database();
-  let storage = firebase.storage();
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let auth = firebase.auth();
+let database = firebase.database();
+let storage = firebase.storage();
+
 
   
-
-  function writeItemData(itemName, price, number, category) {
-    firebase.database().ref('item/' + itemName).set({
-      n: itemName,
-      p: price,
-      nu : number,
-      ca:category
-    });
-  }
-
-  
-  function addNewItem(itemName, price, number, category) {
-    
-    var itemData = {itemName,price,number,category};
+function addNewItem(itemName, price, number, category) {
+    var itemData = {
+        "itemName": itemName,
+        "price": price,
+        "number": number
+    }
 
     // Get a key for a new item.
     var newItemKey = firebase.database().ref().child('item').push().key;
-  
+
     // Write the new item's data simultaneously in the item list and the item's category list.
     var updates = {};
-    updates['/item-category/' + ca + '/item-Name/' + itemName+'/item-Key/'+newItemKey] = itemData;
-  
-    return firebase.database().ref().update(updates);
-  }
+    updates['/book/' + category + '/items/' + newItemKey] = itemData;
 
-  //delete an item
-  function deleteItem(itemKey,number) {
+    return firebase.database().ref().update(updates);
+}
+
+//delete an item
+function deleteItem(itemKey,number) {
     database.ref("/item-Key/"+itemKey).remove();
 }
+
