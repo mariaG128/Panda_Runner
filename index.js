b5df934aab8dd9c801eef3c68d16cdc10d54023f
Michael Zhang <heronalps@gmail.com>
2020-01-30 14:44:29 -0800

    mz - delete account info

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyD3-cWuwLgADptEdheQ179E3_UYQY5tzB4",
-    authDomain: "fir-demo-bea53.firebaseapp.com",
-    databaseURL: "https://fir-demo-bea53.firebaseio.com",
-    projectId: "fir-demo-bea53",
-    storageBucket: "fir-demo-bea53.appspot.com",
-    messagingSenderId: "8929190603",
-    appId: "1:8929190603:web:4f5cd0feb3d9d665c2f14e",
-    measurementId: "G-GE4LPT8MMZ"
+    
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
